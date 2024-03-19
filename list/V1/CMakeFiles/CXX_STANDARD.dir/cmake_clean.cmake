64f9bac2216f6ff5f409b1292e456412feb903df
raznavoyan <razonavoyan17@gmail.com>
2024-03-19 18:58:03 +0400

    random commit

---
+++ list/V1/CMakeFiles/CXX_STANDARD.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CXX_STANDARD.dir/src/List.cpp.o"
+  "CMakeFiles/CXX_STANDARD.dir/src/List.cpp.o.d"
+  "CMakeFiles/CXX_STANDARD.dir/src/main.cpp.o"
+  "CMakeFiles/CXX_STANDARD.dir/src/main.cpp.o.d"
+  "CXX_STANDARD"
+  "CXX_STANDARD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CXX_STANDARD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
